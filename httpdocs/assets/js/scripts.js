9ceab4a5309c0fe9fa43106e1ef31325e178bca6
Theodore Byron <ted@tedbyron.com>
2018-11-14 17:14:18 -0500

    second carousel for client services with updated scripts file

--- httpdocs/assets/js/scripts.js
+++ httpdocs/assets/js/scripts.js
@@ -169,6 +169,31 @@ $(document).ready(function() {
 	}
 	var accordion = new Accordion($('#accordion'), false);
 
+// Second advertising accordion
+  var Accordion2 = function(el, multiple) {
+    this.el = el || {};
+    this.multiple = multiple || false;
+
+    // Variables privadas
+    var links = this.el.find('.link');
+    // Evento
+    links.on('click', {el: this.el, multiple: this.multiple}, this.dropdown)
+  }
+
+  Accordion2.prototype.dropdown = function(e) {
+    var $el = e.data.el;
+      $this = $(this),
+      $next = $this.next();
+
+    $next.slideToggle();
+    $this.parent().toggleClass('open');
+
+    if (!e.data.multiple) {
+      $el.find('.submenu').not($next).slideUp().parent().removeClass('open');
+    };
+  }
+  var accordion2 = new Accordion2($('#accordion2'), false);
+
 	// Show more/less text in FAQ //
 	$(".more-link").click(function(e){
 	  $(this).closest(".FAQ").toggleClass("show-more");
